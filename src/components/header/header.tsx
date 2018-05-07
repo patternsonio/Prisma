d5ac794c6cd2f80bfed0adb04e940568e03768a1
Hannes Diercks <github@xiphe.net>
2018-05-07 17:17:46 +0200

    fix(header): remove shadow
    in order to not cause duplicate bug with pre-rendering
    
    ref https://github.com/ionic-team/stencil/issues/781

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -3,7 +3,6 @@ import { Component, Prop } from '@stencil/core';
 @Component({
   tag: 'pr-header',
   styleUrl: 'header.css',
-  shadow: true,
 })
 export class Header {
   @Prop() headline: string;
@@ -13,18 +12,23 @@ export class Header {
   @Prop() additionalText: string;
   render() {
     return (
-      <header class="header">
-        <pr-logo class="logo" href="https://patternson.io" />
-        <pr-headline hasMargin={false} class="headline">
+      <header class="pr-header">
+        <pr-logo class="pr-header-logo" href="https://patternson.io" />
+        <pr-headline hasMargin={false} class="pr-header-headline">
           {this.headline}
         </pr-headline>
-        <h2 class="subline">{this.subline}</h2>
+        <h2 class="pr-header-subline">{this.subline}</h2>
         {this.buttonText && (
-          <pr-button baem href={this.href} nomargin={true} class="button">
+          <pr-button
+            baem
+            href={this.href}
+            nomargin={true}
+            class="pr-header-button"
+          >
             <span class="pr-button-text">{this.buttonText}</span>
           </pr-button>
         )}
-        <span class="text">{this.additionalText}</span>
+        <span class="pr-header-text">{this.additionalText}</span>
         <slot />
       </header>
     );
